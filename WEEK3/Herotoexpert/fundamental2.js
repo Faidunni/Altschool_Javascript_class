234cbeef14b2a817e55809978ca860e365d68775
Faidunni <hazeezfaizat@gmail.com>
2024-01-16 22:43:34 +0100

    more classes

--- WEEK3/Herotoexpert/fundamental2.js
+++ WEEK3/Herotoexpert/fundamental2.js
@@ -16,20 +16,20 @@ logger();
 logger();
 logger();
 
-function fruitProcessors(apples, oranges) {
-  const juice = `Juice with ${apples} apples and ${oranges} oranges`;
-  return juice;
-}
+// function fruitProcessors(apples, oranges) {
+//   const juice = `Juice with ${apples} apples and ${oranges} oranges`;
+//   return juice;
+// }
 
-fruitProcessors(5, 0);
-const appleJuice = fruitProcessors(5, 0);
-console.log(appleJuice);
-console.log(fruitProcessors(5, 0));
+// fruitProcessors(5, 0);
+// const appleJuice = fruitProcessors(5, 0);
+// console.log(appleJuice);
+// console.log(fruitProcessors(5, 0));
 
-const appleOrangeJuice = fruitProcessors(2, 4);
-console.log(appleOrangeJuice);
+// const appleOrangeJuice = fruitProcessors(2, 4);
+// console.log(appleOrangeJuice);
 
-console.log(fruitProcessors(2, 4));
+// console.log(fruitProcessors(2, 4));
 
 // function declaration
 function calcAge1(birthYear) {
@@ -52,3 +52,48 @@ const age2 = calcAge2(1991);
 console.log(age2);
 
 // Arrow function
+const calcAge3 = (birthYear) => 2037 - birthYear;
+console.log(calcAge3(1991));
+const age3 = calcAge3(1991);
+console.log(age3);
+
+const yearsUntilRetirement = (birthYear, firstName) => {
+  const age = 2037 - birthYear;
+  const retirment = 65 - age;
+  // return retirment;
+  return `${firstName} retires in ${retirment}`;
+};
+
+console.log(yearsUntilRetirement(1991, "Jonas"));
+console.log(yearsUntilRetirement(1980, "Bob"));
+
+// FUNCTION CALLING FUNCTIONS
+function cutFruitPieces(fruit) {
+  return fruit * 4;
+}
+
+function fruitProcessors(apples, oranges) {
+  const applePieces = cutFruitPieces(apples);
+  const orangePieces = cutFruitPieces(oranges);
+
+  const juice = `Juice with ${applePieces} apples and ${orangePieces} oranges`;
+  return juice;
+}
+
+console.log(fruitProcessors(2, 3));
+
+// function cutFruitPieces(apples, oranges) {
+//   return apples * 4, oranges * 4;
+// }
+
+// function fruitProcessors(apples, oranges) {
+//   const applePieces = cutFruitPieces(apples, oranges);
+//   // const orangePieces = cutFruitPieces(oranges);
+
+//   const juice = `Juice with ${applePieces} apples and orange`;
+//   return juice;
+// }
+
+// console.log(fruitProcessors(4, 5));good logical thinking but you will only get one answer, instead of two
+
+const calaAge = function
