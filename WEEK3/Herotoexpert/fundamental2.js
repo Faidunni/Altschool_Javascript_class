260a5d193d23be8c7dc2ecef1486fd338cbc6f6e
Faidunni <hazeezfaizat@gmail.com>
2024-01-16 23:52:43 +0100

    challenge

--- WEEK3/Herotoexpert/fundamental2.js
+++ WEEK3/Herotoexpert/fundamental2.js
@@ -32,24 +32,24 @@ logger();
 // console.log(fruitProcessors(2, 4));
 
 // function declaration
-function calcAge1(birthYear) {
-  return 2037 - birthYear;
-}
+// function calcAge1(birthYear) {
+//   return 2037 - birthYear;
+// }
 
-calcAge1(1991);
-console.log(calcAge1(1991));
+// calcAge1(1991);
+// console.log(calcAge1(1991));
 
-// or
-const age1 = calcAge1(1991);
-console.log(age1);
+// // or
+// const age1 = calcAge1(1991);
+// console.log(age1);
 
 // function expression
-const calcAge2 = function (birthYear) {
-  return 2037 - birthYear;
-};
+// const calcAge2 = function (birthYear) {
+//   return 2037 - birthYear;
+// };
 
-const age2 = calcAge2(1991);
-console.log(age2);
+// const age2 = calcAge2(1991);
+// console.log(age2);
 
 // Arrow function
 const calcAge3 = (birthYear) => 2037 - birthYear;
@@ -57,15 +57,15 @@ console.log(calcAge3(1991));
 const age3 = calcAge3(1991);
 console.log(age3);
 
-const yearsUntilRetirement = (birthYear, firstName) => {
-  const age = 2037 - birthYear;
-  const retirment = 65 - age;
-  // return retirment;
-  return `${firstName} retires in ${retirment}`;
-};
+// const yearsUntilRetirement = (birthYear, firstName) => {
+//   const age = 2037 - birthYear;
+//   const retirment = 65 - age;
+//   // return retirment;
+//   return `${firstName} retires in ${retirment}`;
+// };
 
-console.log(yearsUntilRetirement(1991, "Jonas"));
-console.log(yearsUntilRetirement(1980, "Bob"));
+// console.log(yearsUntilRetirement(1991, "Jonas"));
+// console.log(yearsUntilRetirement(1980, "Bob"));
 
 // FUNCTION CALLING FUNCTIONS
 function cutFruitPieces(fruit) {
@@ -96,4 +96,31 @@ console.log(fruitProcessors(2, 3));
 
 // console.log(fruitProcessors(4, 5));good logical thinking but you will only get one answer, instead of two
 
-const calaAge = function
+const calaAge = function (birthYear) {
+  return 2037 - birthYear;
+};
+
+const yearsretirement = function (birthYear, firstname) {
+  const age = calaAge(birthYear);
+  const retireMent = 65 - age;
+
+  if (retireMent === 0) {
+    console.log(`${firstname} retires in ${retireMent} years`);
+    return retireMent;
+  } else {
+    console.log(`${firstname} has already retired`);
+    return -1;
+  }
+};
+
+const retirementYears = yearsretirement(1991, "Mike");
+console.log(retirementYears);
+
+// coding Challenge
+const calcAverage = (dolphsin) => (a, b, c) / 3;
+console.log(calcAverage(23 + 20 + 24));
+
+// function teamsAverage(dolphins, koalas) {
+//   const dolphinsAverage = calcAverage(dolphins);
+//   const koalasAverage = calcAverage(koalas);
+// }
