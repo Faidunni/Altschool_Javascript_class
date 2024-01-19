582ff10071aa4bd6e6301b8cd644c0619eae4aba
Faidunni <hazeezfaizat@gmail.com>
2024-01-19 23:14:47 +0100

    more

--- WEEK3/Herotoexpert/fundamental2.js
+++ WEEK3/Herotoexpert/fundamental2.js
@@ -136,25 +136,38 @@ const checkWinner = function (avgDolphins, avgKoalas) {
 checkWinner(dolphinsAverage, koalasAverage);
 
 // ARRAYS
-const friends = ["Michael", "Steven", "Peter"];
-console.log(friends);
-console.log(friends[0]);
-console.log(friends.length);
-console.log(friends[friends.length - 1]);
+// const friends = ["Michael", "Steven", "Peter"];
+// console.log(friends);
+// console.log(friends[0]);
+// console.log(friends.length);
+// console.log(friends[friends.length - 1]);
 
-friends[2] = "Jay";
-console.log(friends);
+// friends[2] = "Jay";
+// console.log(friends);
 
-const firstName = "Jonas";
-const jonas = [firstName, "Schmed", 2037 - 1991, "teacher", friends];
-console.log(jonas);
+// const firstName = "Jonas";
+// const jonas = [firstName, "Schmed", 2037 - 1991, "teacher", friends];
+// console.log(jonas);
 
-const years = new Array(1991, 1984, 1776);
-console.log(years);
+// const y = new Array(1991, 1984, 1776);
+// console.log(y);
 
 // array exercise
 const calcAge = function (birthYear) {
   return 2037 - birthYear;
 };
-const Years = [1990, 1967, 2002, 2012, 2010];
-console.log(calaAge(Years));
+
+const years = [1990, 1967, 2002, 2012, 2010];
+console.log(calcAge(years)); //this will produce not a number
+
+const age1 = calcAge(years[0]);
+const age2 = calcAge(years[1]);
+const age4 = calcAge(years[years.length - 1]);
+console.log(age1, age2, age4);
+
+const ages = [
+  calaAge(years[0]),
+  calcAge(years[1]),
+  calcAge(years[years.length - 1]),
+];
+console.log(ages);
